44fce25dbbc092b7dc564ba349ec45ac04fe7286
H. Kriti <hirekerurkriti@gmail.com>
2025-10-24 19:37:28 +0530

    minor: add port config

--- server/app.py
+++ server/app.py
@@ -49,4 +49,4 @@ def update_form():
         return render_template('error.html', error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 8080))
